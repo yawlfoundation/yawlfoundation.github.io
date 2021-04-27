94f79a395aaea240e40b63e3eef3280bc814d7d7
Michael Adams <mj.adams@qut.edu.au>
2021-04-27 12:03:13 +1000

    removed fopas link

--- assets/mbr-switch-arrow/mbr-switch-arrow.js
+++ assets/mbr-switch-arrow/mbr-switch-arrow.js
@@ -1,41 +1,4 @@
-var isBuilder = $('html').hasClass('is-builder');
-if (!isBuilder) {
-    if (typeof window.initSwitchArrowPlugin === 'undefined'){
-        window.initSwitchArrowPlugin = true;
-        $(document).ready(function() {
-            if ($('.accordionStyles').length!=0) {
-                    $('.accordionStyles .card-header a[role="button"]').each(function(){
-                        if(!$(this).hasClass('collapsed')){
-                            $(this).addClass('collapsed');
-                        }
-                    });
-                }
-        });
 
-        $('.accordionStyles .card-header a[role="button"]').click(function(){
-            var $id = $(this).closest('.accordionStyles').attr('id'),
-                $iscollapsing = $(this).closest('.card').find('.panel-collapse');
-
-            if (!$iscollapsing.hasClass('collapsing')) {
-                if ($id.indexOf('toggle') != -1){
-                    if ($(this).hasClass('collapsed')) {
-                        $(this).find('span.sign').removeClass('mbri-arrow-down').addClass('mbri-arrow-up'); 
-                    }
-                    else{
-                        $(this).find('span.sign').removeClass('mbri-arrow-up').addClass('mbri-arrow-down'); 
-                    }
-                }
-                else if ($id.indexOf('accordion')!=-1) {
-                    var $accordion =  $(this).closest('.accordionStyles ');
-                
-                    $accordion.children('.card').each(function() {
-                        $(this).find('span.sign').removeClass('mbri-arrow-up').addClass('mbri-arrow-down'); 
-                    });
-                    if ($(this).hasClass('collapsed')) {
-                        $(this).find('span.sign').removeClass('mbri-arrow-down').addClass('mbri-arrow-up'); 
-                    }
-                }
-            }
-        });
-    }
-};
+var isBuilder=$("html").hasClass("is-builder");
+isBuilder||"undefined"!==typeof window.initSwitchArrowPlugin||(window.initSwitchArrowPlugin=!0,$(document).ready(function(){0!=$(".accordionStyles").length&&$('.accordionStyles .card-header a[role="button"]').each(function(){$(this).hasClass("collapsed")||$(this).addClass("collapsed")})}),$('.accordionStyles .card-header a[role="button"]').click(function(){var a=$(this).closest(".accordionStyles").attr("id");$(this).closest(".card").find(".panel-collapse").hasClass("collapsing")||(-1!=a.indexOf("toggle")?
+$(this).hasClass("collapsed")?$(this).find("span.sign").removeClass("mbri-arrow-down").addClass("mbri-arrow-up"):$(this).find("span.sign").removeClass("mbri-arrow-up").addClass("mbri-arrow-down"):-1!=a.indexOf("accordion")&&($(this).closest(".accordionStyles ").children(".card").each(function(){$(this).find("span.sign").removeClass("mbri-arrow-up").addClass("mbri-arrow-down")}),$(this).hasClass("collapsed")&&$(this).find("span.sign").removeClass("mbri-arrow-down").addClass("mbri-arrow-up")))}));
